025cc43f73443d5f159225dc9d96b18815cf9779
robert.schneider <robert.schneider@stackpath.com>
2022-03-01 23:58:43 -0500

    Added boilder plate and reaction event

--- main.go
+++ main.go
@@ -1,15 +1,62 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+// Variables used for command line parameters
+var (
+	Token string
+)
+
+func init() {
+
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
 func main() {
 
-	discordToken := os.Getenv("DISCORD_TOKEN")
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
 
-	discordgo.New(discordToken)
+	// Register the reactionAdd func as a callback for MessageReactionAdd events.
+	dg.AddHandler(reactionAdd)
+
+	// Set Identity Intent for MessageReactions
+	dg.Identify.Intents = discordgo.IntentsGuildMessageReactions
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func reactionAdd(s *discordgo.Session, e *discordgo.MessageReactionAdd) {
+
+	if e.Emoji.Name == "🥰" {
+		fmt.Println("Hello")
+	}
 
 }
