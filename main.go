127127efc32a27ccf7f8aa53c1a865efabcc5b5d
robert.schneider <robert.schneider@stackpath.com>
2022-03-02 16:24:26 -0500

    Adds signs logic

--- main.go
+++ main.go
@@ -53,19 +53,51 @@ func main() {
 	dg.Close()
 }
 
-var emojiMap = map[string]string{
+var pronounMap = map[string]string{
 	"🦋": "761015648494288926", // they/them
 	"🐝": "761016805229461505", // she/her
 	"🪲": "761016870286655509", // he/him
+}
+
+var cosmoMap = map[string]string{
+	"♈": "874347037167058964", // Aries
+	"♉": "874347209401974824", // Taurus
+	"♊": "874347353979645993", // Gemini
+	"♋": "874347521978286141", // Cancer
+	"♌": "874347602487955476", // Leo
+	"♍": "874348150893183066", // Virgo
+	"♎": "874348259563413566", // Libra
+	"♏": "874348351221551164", // Scorpio
+	"♐": "874348449674461185", // Sagittarius
+	"♑": "874348499142078495", // Capricorn
+	"♒": "874348617777967106", // Aquarius
+	"♓": "874348705539563531", // Pisces
+}
+
+var pronounsMessage = "866078211628073011"
+var signsMessage = "874352686408007690"
+
+func emojiCheck(emoji string) (string, bool) {
+	value1, found1 := pronounMap[emoji]
+	value2, found2 := cosmoMap[emoji]
+
+	if found1 {
+		return value1, true
+	} else if found2 {
+		return value2, true
+	} else {
+		return "None", false
+	}
 }
 
 // Capture reactions added
 func reactionAdd(s *discordgo.Session, e *discordgo.MessageReactionAdd) {
 
 	// Check to see if emoji is our list
-	value, ok := emojiMap[e.Emoji.Name]
+	value, ok := emojiCheck(e.Emoji.Name)
 	if ok {
-		if e.MessageID == "866078211628073011" {
+		// 				  Message ID for pronouns				 Message ID for pronouns
+		if e.MessageID == pronounsMessage || e.MessageID == signsMessage {
 			// if added change Role
 			s.GuildMemberRoleAdd(e.GuildID, e.UserID, value)
 		}
@@ -76,9 +108,9 @@ func reactionAdd(s *discordgo.Session, e *discordgo.MessageReactionAdd) {
 func reactionRemove(s *discordgo.Session, e *discordgo.MessageReactionRemove) {
 
 	// Check to see if emoji is our list
-	value, ok := emojiMap[e.Emoji.Name]
+	value, ok := emojiCheck(e.Emoji.Name)
 	if ok {
-		if e.MessageID == "866078211628073011" {
+		if e.MessageID == pronounsMessage || e.MessageID == signsMessage {
 			// if removed change Role
 			s.GuildMemberRoleRemove(e.GuildID, e.UserID, value)
 		}
