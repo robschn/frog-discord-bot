1975d6c76e4e286726f0ab44afd8875fd1069324
robert.schneider <robert.schneider@stackpath.com>
2022-03-12 18:17:02 -0500

    Adds comments for code

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
 func main() {
+	// authenticate and register the handler functions
 	auth()
+
+	// check to see if we can run poll
+	poll()
 }
--- poll.go
+++ poll.go
@@ -1,1 +1,47 @@
 package main
+
+// send message that Satan off is starting on Sunday
+func startSatanOff(timeToStart string) bool {
+	// start when time condition is met
+	var shouldStart bool
+	// return bool for whether the contest should start
+	return shouldStart
+}
+
+// grab participants
+func registerContestants() []string {
+	// send message for users to register
+	// these will be users that show in poll
+	// possibly allow to choose their own emoji
+	// cannot vote
+
+	// Leave voting up for 30 minutues
+
+	// Warn users at 2 mins
+
+	// Tell chat that voting is closed and display contestants
+	// Wish good luck to participants and chat
+	var registeredUsers []string
+	// return a slice of registered user
+	return registeredUsers
+}
+
+// Around 5PM open voting
+func runVoting() []string {
+	// display participants
+
+	// For every person, assign them to an emoji
+
+	// after an hour, count the emojis and display winner
+	// maybe pin post
+	// if tie, the each tie wins
+
+	// change role to:
+	// Literally Lucifer - One winner
+	// Co-Satan - Two or more winners
+	// should leverage the role.go we made
+	// we're returning a slice for more than one winner
+	var userWinner []string
+	//
+	return userWinner
+}
