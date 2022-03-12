6621995d5724bf88d9f0e18bf8187ab8fa1c0c37
robert.schneider <robert.schneider@stackpath.com>
2022-03-12 18:38:59 -0500

    Moves satanOff to own thread

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
 
-import "time"
-
 func main() {
 	// authenticate and register the handler functions
 	auth()
 
-	// check to see if we can run poll
-	if startSatanOff("Time example here") {
-		// get user list
-		registerContestants()
-
-		// run voting
-		winnerUser := runVoting()
-
-		// change the role from returned voting
-		changeRole(winnerUser)
-
-	} else {
-		time.Sleep(30 * time.Minute)
-	}
+	go runSatanOff()
 }
