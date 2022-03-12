d6ec657e8a4c02edacf74beef3ca1ebf48efb55f
robert.schneider <robert.schneider@stackpath.com>
2022-03-12 18:21:41 -0500

    Adds poll func to main

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
+import "time"
+
 func main() {
 	// authenticate and register the handler functions
 	auth()
 
 	// check to see if we can run poll
-	poll()
+	if startSatanOff("Time example here") {
+		// get user list
+		registerContestants()
+
+		// run voting
+		winnerUser := runVoting()
+
+		// change the role from returned voting
+		changeRole(winnerUser)
+
+	} else {
+		time.Sleep(30 * time.Minute)
+	}
 }
