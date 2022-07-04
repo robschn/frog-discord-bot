d5ec4c0252d3754d06e8a4e04694821701f1cac9
Rob Schneider <robmschn@gmail.com>
2022-07-04 19:54:16 -0400

    Adds update to global var

--- poll.go
+++ poll.go
@@ -12,7 +12,7 @@ func listenForPoll(s *discordgo.Session, e *discordgo.MessageCreate) {
 	if e.ChannelID == "864719238279462912" {
 		if e.Content == "!poll movie" {
 			// send message to user asking for the names of the 3 movies
-			messagePollAuthor := e.Author.ID
+			messagePollAuthor = e.Author.ID
 			messageResponse := fmt.Sprintf("<@%s> please type in 3 movie names separated by commas.", messagePollAuthor)
 			s.ChannelMessageSend(e.ChannelID, messageResponse)
 		}
