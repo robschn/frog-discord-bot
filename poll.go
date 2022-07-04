c8f0863ff80cc3df930ebe37f021f74731d60aac
robert.schneider <robert.schneider@stackpath.com>
2022-07-04 19:48:33 -0400

    Adds listener

--- poll.go
+++ poll.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
 
-// https://github.com/jamestjw/lyrical/blob/master/poll/poll.go
-func listenForPoll(s *discordgo.Session, e *discordgo.MessageReactionAdd) {
+	"github.com/bwmarrin/discordgo"
+)
 
+var messagePollAuthor string
+
+func listenForPoll(s *discordgo.Session, e *discordgo.MessageCreate) {
+	if e.ChannelID == "864719238279462912" {
+		if e.Content == "!poll movie" {
+			// send message to user asking for the names of the 3 movies
+			messagePollAuthor := e.Author.ID
+			messageResponse := fmt.Sprintf("<@%s> please type in 3 movie names separated by commas.", messagePollAuthor)
+			s.ChannelMessageSend(e.ChannelID, messageResponse)
+		}
+		if e.Author.ID == messagePollAuthor {
+			s.ChannelMessageSend(e.ChannelID, "Hey")
+		}
+	}
 }
