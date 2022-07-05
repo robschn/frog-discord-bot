2ebb9c69bd012feb119f32d28884013b6a41ff30
robert.schneider <robert.schneider@stackpath.com>
2022-07-04 22:53:27 -0400

    Adds random movie picks

--- poll.go
+++ poll.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -10,14 +15,69 @@ var messagePollAuthor string
 
 func listenForPoll(s *discordgo.Session, e *discordgo.MessageCreate) {
 	if e.ChannelID == "864719238279462912" {
+		// check to see if messagePollAuthor is set
+		if e.Author.ID == messagePollAuthor {
+			jsonToPoll(3)
+		}
+		// check for poll command
 		if e.Content == "!poll movie" {
 			// send message to user asking for the names of the 3 movies
 			messagePollAuthor = e.Author.ID
 			messageResponse := fmt.Sprintf("<@%s> please type in 3 movie names separated by commas.", messagePollAuthor)
 			s.ChannelMessageSend(e.ChannelID, messageResponse)
 		}
-		if e.Author.ID == messagePollAuthor {
-			s.ChannelMessageSend(e.ChannelID, "Hey")
+	}
+}
+
+func jsonToPoll(limit int) []string {
+	// initalize struct for JSON
+	type Movie struct {
+		Name    string `json:"name"`
+		Year    int    `json:"year"`
+		Watched bool   `json:"watched"`
+	}
+
+	type Movies struct {
+		Movies []Movie `json:"movies"`
+	}
+
+	// auth to GitHub Gist API
+
+	// grab current lists
+	moviesJson, err := os.Open("movies.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := ioutil.ReadAll(moviesJson)
+
+	var movies Movies
+
+	// add JSON to movies list
+	json.Unmarshal(byteValue, &movies)
+
+	var unwatchedMovies []Movie
+
+	// exclude watched moviess
+	for _, mov := range movies.Movies {
+		if !(mov.Watched) {
+			unwatchedMovies = append(unwatchedMovies, mov)
 		}
 	}
+
+	var randIndex []int
+
+	// get random indexes
+	rand.Seed(time.Now().UnixNano())
+	p := rand.Perm(len(unwatchedMovies))
+	randIndex = append(randIndex, p...)
+
+	var pickedMovies []string
+
+	// loop over the first 3 random indexes
+	for _, i := range randIndex[0:3] {
+		movieInfo := fmt.Sprintf("%s (%v)", unwatchedMovies[i].Name, unwatchedMovies[i].Year)
+		pickedMovies = append(pickedMovies, movieInfo)
+	}
+
+	return pickedMovies
 }
