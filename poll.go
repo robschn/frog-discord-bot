fba97c98a0fe9d96f46d65af2e085488b5381339
robschn <robmschn@gmail.com>
2022-08-08 23:23:59 -0400

    Adds redis for movies

--- poll.go
+++ poll.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
-	"io/ioutil"
-	"math/rand"
 	"os"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-redis/redis/v9"
 )
 
 func listenForPoll(s *discordgo.Session, e *discordgo.MessageCreate) {
@@ -16,7 +15,7 @@ func listenForPoll(s *discordgo.Session, e *discordgo.MessageCreate) {
 		// check for poll command
 		if e.Content == "!poll movie" {
 			// send message to user asking for the names of the 3 movies
-			pickedMovies := jsonToPoll(3)
+			pickedMovies := fetchMovies(3)
 
 			// format to add 7 days to current date
 			nextWeek := time.Now().AddDate(0, 0, 6)
@@ -38,10 +37,11 @@ Voting ends at midnight on Sunday.`
 
 			// add emojis to message
 			emojiHash := map[string]string{
-				"🧡": pickedMovies[0],
-				"💛": pickedMovies[1],
-				"💚": pickedMovies[2],
+				"🧡": pickedMovies[0].Value,
+				"💛": pickedMovies[1].Value,
+				"💚": pickedMovies[2].Value,
 			}
+
 			for i := range emojiHash {
 				s.MessageReactionAdd(messageInfo.ChannelID, messageInfo.ID, i)
 			}
@@ -61,58 +61,22 @@ Voting ends at midnight on Sunday.`
 			winnerMovie := fmt.Sprintf("The MovieMonday winner is %s !", emojiHash[emojiCheck.Reactions[0].Emoji.Name])
 			s.ChannelMessageSend(messageInfo.ChannelID, winnerMovie)
 		}
+		if e.Content == "!poll upload" {
+
+		}
 	}
 }
 
-func jsonToPoll(limit int) []string {
-	// initalize struct for JSON
-	type Movie struct {
-		Name    string `json:"name"`
-		Year    int    `json:"year"`
-		Watched bool   `json:"watched"`
-	}
+func fetchMovies(limit int) []redis.KeyValue {
 
-	type Movies struct {
-		Movies []Movie `json:"movies"`
-	}
+	ctx := context.TODO()
 
-	// auth to GitHub Gist API
+	// connect to redis database
+	r := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: os.Getenv("REDIS_PASS"),
+		DB:       0,
+	})
 
-	// grab current lists
-	moviesJson, err := os.Open("movies.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	byteValue, _ := ioutil.ReadAll(moviesJson)
-
-	var movies Movies
-
-	// add JSON to movies list
-	json.Unmarshal(byteValue, &movies)
-
-	var unwatchedMovies []Movie
-
-	// exclude watched moviess
-	for _, mov := range movies.Movies {
-		if !(mov.Watched) {
-			unwatchedMovies = append(unwatchedMovies, mov)
-		}
-	}
-
-	var randIndex []int
-
-	// get random indexes
-	rand.Seed(time.Now().UnixNano())
-	p := rand.Perm(len(unwatchedMovies))
-	randIndex = append(randIndex, p...)
-
-	var pickedMovies []string
-
-	// loop over the random indexes from 0 to limit
-	for _, i := range randIndex[0:limit] {
-		movieInfo := fmt.Sprintf("%s (%v)", unwatchedMovies[i].Name, unwatchedMovies[i].Year)
-		pickedMovies = append(pickedMovies, movieInfo)
-	}
-
-	return pickedMovies
+	return r.HRandFieldWithValues(ctx, "unwatched", limit).Val()
 }
